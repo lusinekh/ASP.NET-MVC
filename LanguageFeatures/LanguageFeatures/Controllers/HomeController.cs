538ecc9b590df07c00d3fe83665e90fc9fe18d66
User <User@DESKTOP-I87ABNR>
2019-04-09 05:46:25 -0700

    Add LanguageFeatures

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+            // set the property value
+            myProduct.Name = "Kayak";
+            // get the property
+            string productName = myProduct.Name;
+            // generate the view
+            return View("Result",
+            (object)String.Format("Product name: {0}", productName));
+        }
+
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+            return View("Result",
+            (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+
+    }
+}
